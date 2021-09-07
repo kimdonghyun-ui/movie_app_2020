0a162ec1c64e6a8c6449429eb898b596c40548af
dongdong <dongdong@eyesvision.com>
2021-09-07 17:51:18 +0900

    189p 시작하면됨

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component {
   state = {
@@ -11,22 +13,41 @@ class App extends React.Component {
       data: {
         data: {movies},
       }
-    } = await axios.get('https://yts-proxy.now.sh/list_movies.json');
-    // console.log(movies);
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    //console.log(movies);
     this.setState({ movies,isLoading: false });
   }
 
   componentDidMount() {
     //영화 데이터 로딩!
     //https://yts.mx/api/v2/movie_details.json?movie_id=1
+    //https://yts.mx/api/v2/list_movies.json?sort_by=rating
     this.getMovies();
   }
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
     return (
-      <div>
-        { isLoading ? 'Loading...' : 'We are ready' }
-      </div>
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+            <div className="movies">
+              {movies.map((movie) => (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              ))}
+            </div>
+        )
+      }
+      </section>
     )
   }
 }
